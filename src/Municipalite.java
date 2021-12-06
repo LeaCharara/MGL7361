b4825f0b97091b52c46a32999ae52343d6259206
Lea Charara <leacharara@hotmail.com>
2021-12-06 17:46:53 -0500

    Modifier id dans municipalite

--- src/Municipalite.java
+++ src/Municipalite.java
@@ -7,7 +7,7 @@ public class Municipalite implements IGestionAvantage {
 	private HashMap<Integer, Avantage> avantages;
 	public Municipalite(String nom) {
 		super();
-		this.id = id;
+		this.id = (int)(Math.random() * 828829);
 		this.nom = nom;
 		avantages = new HashMap<Integer, Avantage>();
 		addAvantage(new Avantage("45 minutes de stationnement gratuit"));
